85367a60db3462f24a52829d5b63dd739093bb0e
kayque <kayquegps2@gmail.com>
2022-01-13 11:57:41 -0300

    adding css to h1 and img tags in the home component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,8 @@ import "./Home.css"
 function Home(){
     return(
         <>
-            <h1>Home</h1>
+            <h1 className="title ">Home</h1>
+            <img className="img" src="" alt="" />
         </>
     );
 }
