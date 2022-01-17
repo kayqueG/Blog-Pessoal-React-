ff61d88a7793d62d6bb4180d51d61020eef4d378
kayque <kayquegps2@gmail.com>
2022-01-17 08:52:41 -0300

    add grid

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
+import { Grid, Paper } from "@material-ui/core";
 
-function Home(){
-    return(
-        <>
-            <h1 className="title ">Home</h1>
-            <img className="img" src="https://i.imgur.com/H88yIo2.png" alt="Imagem tela inicial" />
-        </>
-    );
+function Home() {
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={8}>
+          <Paper style={{height:"100vh",background:"lightgrey"}}/>
+        </Grid>
+        <Grid item container direction="column" xs={12} sm={4} spacing={2}>
+          <Grid item>
+            <Paper style={{height:"49vh",background:"orange"}} />
+          </Grid>
+          <Grid item>
+            <Paper style={{height:"49vh",background:"green"}} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default Home;
