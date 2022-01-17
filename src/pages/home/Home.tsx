c564ed571d47a624ba2a2c4d6f40151dba5044ed
kayque <kayquegps2@gmail.com>
2022-01-17 09:15:26 -0300

    add flexbox and button

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import "./Home.css";
-import { Grid, Paper } from "@material-ui/core";
+import { Grid, Paper,Box,Button } from "@material-ui/core";
 
 function Home() {
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={8}>
-          <Paper style={{height:"100vh",background:"lightgrey"}}/>
-        </Grid>
-        <Grid item container direction="column" xs={12} sm={4} spacing={2}>
-          <Grid item>
-            <Paper style={{height:"49vh",background:"orange"}} />
-          </Grid>
-          <Grid item>
-            <Paper style={{height:"49vh",background:"green"}} />
-          </Grid>
-        </Grid>
-      </Grid>
+        <Paper>
+            <Box p={2}>
+                <Box display="flex" justifyContent="center">
+                    <h1>Título</h1>
+                </Box>
+                <img src="" alt="" style={{width:"100%",height:"100%"}}/>
+                <Box display="flex" justifyContent="center" p={2}>
+                    <Button color="primary" variant="contained">Texto1</Button>
+                    <Button color="secondary" variant="contained" > Texto2</Button></Box>
+            </Box>
+        </Paper>
     </>
   );
 }
